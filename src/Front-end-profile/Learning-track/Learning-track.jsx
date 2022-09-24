df4c93a8b32a9a2cd64e63684707c061a9bb8a88
Bilen Ray <97880050+bilenray@users.noreply.github.com>
2022-09-25 00:56:04 +1000

    updating-learning-track-html-file
    learning track html file

--- src/Front-end-profile/Learning-track/Learning-track.jsx
+++ src/Front-end-profile/Learning-track/Learning-track.jsx
@@ -5,24 +5,50 @@ const LearningTrack = () => {
     <div className='lt-wrapper'>
         <div className='lt-react-wrapper'>
             <div className='lt-react-track'>
-                <div className='lt-html-css-js'>HTML, CSS and JS</div>
-                <div className='lt-react-concept-jsx'>React concept and JSX</div>
-                <div className='lt-hooks'>React hooks</div>
-                <div className='props-props-drilling'>Props, Props-drilling and State Management</div>
-                <div className='lt-composition-advance-hooks'>Composition and Advance Hooks</div>
-                <div className='lt-react-query-api-call'>React Query</div>
+                <div className='lt-hover-control'>
+                    <div data-hover="From basic to advance" className='lt-html-css-js hovertext z'>HTML, CSS and JS</div>
+                </div>
+                <div className='lt-hover-control'>
+                    <div data-hover="From basic to advance" className='lt-react-concept-jsx hovertext z'>React concept and JSX</div>
+                </div>
+                <div className='lt-hover-control'>
+                    <div data-hover="From basic to advance" className='lt-hooks hovertext z'>React hooks</div>
+                </div>
+                <div className='lt-hover-control'>
+                    <div data-hover="From basic to advance" className='lt-props-drilling hovertext z'>Props, Props-drilling and State Management</div>
+                </div>
+                <div className='lt-hover-control'>
+                    <div data-hover="From basic to advance" className='lt-composition-advance-hooks hovertext z'>Composition and Advance Hooks</div>
+                </div>
+                <div className='lt-hover-control'>
+                    <div data-hover="From basic to advance" className='lt-react-query-api-call hovertext z'>React Query</div>
+                </div>
             </div>
             <div className='lt-programming'>React.js</div>
         </div>
         <div className='lt-node-wrapper'>
             <div className='lt-node-track'>
-                <div className='lt-basics'>basics</div>
-                <div className='lt-crud'>CRUD</div>
-                <div className='lt-socket'>Socket</div>
-                <div className='lt-apicall-handle'>API call</div>
-                <div className='lt-nosql-sql'>NO-SQL and SQL</div>
-                <div className='lt-cloud'>Cloud</div>
-                <div className='lt-largedata-security'>Large Data and Security</div>
+                <div className='lt-hover-control'>
+                    <div data-hover="From basic to advance" className='lt-basics hovertext z'>basics</div>
+                </div>
+                <div className='lt-hover-control'>
+                    <div data-hover="From basic to advance" className='lt-crud hovertext z'>CRUD</div>
+                </div>
+                <div className='lt-hover-control'>
+                    <div data-hover="From basic to advance" className='lt-socket hovertext z'>Socket</div>
+                </div>
+                <div className='lt-hover-control'>
+                    <div data-hover="From basic to advance" className='lt-apicall-handle hovertext z'>API call</div>
+                </div>
+                <div className='lt-hover-control'>
+                    <div data-hover="From basic to advance" className='lt-nosql-sql hovertext z'>NO-SQL and SQL</div>
+                </div>
+                <div className='lt-hover-control'>
+                    <div data-hover="From basic to advance" className='lt-cloud hovertext z'>Cloud</div>
+                </div>
+                <div className='lt-hover-control'>
+                    <div data-hover="From basic to advance" className='lt-largedata-security hovertext z'>Large Data and Security</div>
+                </div>
             </div>
             <div className='lt-programming'>NODE.js</div>
         </div>
