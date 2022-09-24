0d361896cee0e32d81ed43ce5bb99eec87c3af15
bilenray <raybimlendra@gmail.com>
2022-09-24 08:57:59 -0700

    adding-image

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Introduction from './Front-end-profile/Introduction/Introduction'
 import LearningTrack from './Front-end-profile/Learning-track/Learning-track'
 import Links from './Front-end-profile/Links/Links';
+import Skill from './Front-end-profile/Skills/Skill';
 function App() {
   return (
     <div className="App">
       <Introduction/>
       <LearningTrack/>
       <Links/>
+      <Skill/>
     </div>
   );
 }
